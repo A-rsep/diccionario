a6faa3bf2dd437ba89787bf495bd94246afa5788
A-rsep <revid7798@gmail.com>
2025-03-01 12:38:16 -0300

    Update diccionario_adultos.py

--- diccionario_adultos.py
+++ diccionario_adultos.py
@@ -2,7 +2,7 @@ meme_dict = {
             "CRINGE": "algo excepcionalmente raro o embarazoso",
             "LOL": "una respuesta común a algo gracioso",
             "VOID": "algo que tiene un contexto turbio",
-            "SKIBIDI": "es una palabra que hace referencia a una serie, no tiene un significado como tal"
+            "SKIBIDI": "es una palabra que hace referencia a una serie animada web, no tiene un significado como tal"
             }
 
 word = input("Escribe una palabra que no entiendas: ").upper()
@@ -11,3 +11,4 @@ if word in meme_dict.keys():
    print("Esto significa que es", meme_dict[word])
 else:
     print('Lo sentimos pero la palabra no está en el diccionario')
+
